ad78cbabf93f2ccb2582693ec7c483769fd9be1c
valfink <117605562+valfink@users.noreply.github.com>
2023-06-20 15:39:02 +0200

    Use new SpecificationFilter

--- open-closed/src/main/java/org/example/Main.java
+++ open-closed/src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.filter_specification.AndSpecification;
+import org.example.filter_specification.ColorSpecification;
+import org.example.filter_specification.SizeSpecification;
+import org.example.filter_specification.SpecificationFilter;
+
 import java.util.List;
 
 public class Main {
@@ -10,9 +15,29 @@ public class Main {
 
         List<Product> products = List.of(apple, tree, house);
 
+
+        // Bad way, code needed to be modified for different filter criteria
+
         BadProductFilter badProductFilter = new BadProductFilter();
         System.out.println("Green products (bad filter):");
         badProductFilter.filterByColor(products, Color.GREEN)
+                .forEach(p -> System.out.println(" - " + p.name()));
+
+
+        // Good way, with code open for extension and closed for modification
+
+        SpecificationFilter specificationFilter = new SpecificationFilter();
+
+        System.out.println("Green products (good filter):");
+        specificationFilter.filter(products, new ColorSpecification(Color.GREEN))
+                .forEach(p -> System.out.println(" - " + p.name()));
+        System.out.println("Large products (good filter):");
+        specificationFilter.filter(products, new SizeSpecification(Size.LARGE))
+                .forEach(p -> System.out.println(" - " + p.name()));
+        System.out.println("Large red products (good filter):");
+        specificationFilter.filter(products, new AndSpecification<>(
+                new SizeSpecification(Size.LARGE), new ColorSpecification(Color.RED)
+        ))
                 .forEach(p -> System.out.println(" - " + p.name()));
     }
 }
