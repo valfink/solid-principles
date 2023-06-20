6c6d38c606dfc9df5543593e33e414215bc45da6
valfink <117605562+valfink@users.noreply.github.com>
2023-06-20 15:59:53 +0200

    Add Rectangle and Square (which breaks Liskov substitution principle)

--- liskov-substitution/src/main/java/org/example/Main.java
+++ liskov-substitution/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
+
+    static void useRectangle(Rectangle rectangle) {
+        int width = rectangle.getWidth();
+        rectangle.setHeight(10);
+        System.out.println("Expecting area of " + (width*10) + ", getting " + rectangle.getArea());
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rectangle rectangle = new Rectangle(2, 3);
+        useRectangle(rectangle);
+
+        // Square breaks the math!
+        Square square = new Square(5);
+        useRectangle(square);
     }
 }
