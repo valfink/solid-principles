8e0ffe0b3352852a6a2fca5902a38322397f6bed
valfink <117605562+valfink@users.noreply.github.com>
2023-06-20 15:10:43 +0200

    Add call to BadProductFilter

--- open-closed/src/main/java/org/example/Main.java
+++ open-closed/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.RED, Size.LARGE);
+
+        List<Product> products = List.of(apple, tree, house);
+
+        BadProductFilter badProductFilter = new BadProductFilter();
+        System.out.println("Green products (bad filter):");
+        badProductFilter.filterByColor(products, Color.GREEN)
+                .forEach(p -> System.out.println(" - " + p.name()));
     }
 }
