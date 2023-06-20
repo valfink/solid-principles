0abddd7c4150328183a8f12827ace739387b8148
valfink <117605562+valfink@users.noreply.github.com>
2023-06-20 15:06:50 +0200

    Add new filter criteria, modifying the Class (against Open-Closed principle)

--- open-closed/src/main/java/org/example/BadProductFilter.java
+++ open-closed/src/main/java/org/example/BadProductFilter.java
@@ -8,4 +8,10 @@ public class BadProductFilter {
         return products.stream().filter(p -> p.color().equals(color));
     }
 
+    // against Open-Closed: now we are modifying the class to add another filter
+    public Stream<Product> filterBySize(List<Product> products, Size size) {
+        return products.stream().filter(p -> p.size().equals(size));
+    }
+
+    // this would go on for every added filter criteria!
 }
