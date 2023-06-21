48f1c020368b9d8ab4741096c8cced1e7c2e0aa8
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 10:19:11 +0200

    Add dependency inverted & abstracted RelationshipBrowser, Relationships, Research

--- dependency-inversion/src/main/java/org/example/dependency_inversion/Relationships.java
+++ dependency-inversion/src/main/java/org/example/dependency_inversion/Relationships.java
@@ -8,16 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Relationships implements RelationshipBrowser {
-    private final List<Triplet<Person, Relationship, Person>> relationships = new ArrayList<>();
+    private final List<Triplet<Person, Relationship, Person>> relationshipList = new ArrayList<>();
 
     public void addParentAndChild(Person parent, Person child) {
-        relationships.add(new Triplet<>(parent, Relationship.PARENT, child));
-        relationships.add(new Triplet<>(child, Relationship.CHILD, parent));
+        relationshipList.add(new Triplet<>(parent, Relationship.PARENT, child));
+        relationshipList.add(new Triplet<>(child, Relationship.CHILD, parent));
     }
 
     @Override
     public List<Person> findChildrenOf(String name) {
-        return relationships.stream()
+        return relationshipList.stream()
                 .filter(r -> r.getValue0().name().equals(name) && r.getValue1() == Relationship.PARENT)
                 .map(Triplet::getValue2)
                 .toList();
