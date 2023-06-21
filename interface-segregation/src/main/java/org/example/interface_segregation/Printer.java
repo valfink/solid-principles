6b3391df1e51a013c6b2130e7ac68522610590b4
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 08:53:15 +0200

    Add segregated interfaces Printer & Scanner

---
+++ interface-segregation/src/main/java/org/example/interface_segregation/Printer.java
@@ -0,0 +1,7 @@
+package org.example.interface_segregation;
+
+import org.example.Document;
+
+public interface Printer {
+    void print(Document document);
+}
---
+++ interface-segregation/src/main/java/org/example/interface_segregation/Scanner.java
@@ -0,0 +1,7 @@
+package org.example.interface_segregation;
+
+import org.example.Document;
+
+public interface Scanner {
+    void scan(Document document);
+}
