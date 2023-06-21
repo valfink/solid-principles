3b16f25b4cafb6fca318c8465f1aabd7bcc591be
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 09:39:44 +0200

    Add BadRelationships

--- dependency-inversion/src/main/java/org/example/BadRelationships.java
+++ dependency-inversion/src/main/java/org/example/BadRelationships.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class BadRelationships {
     // low-level module - no business logic, only for storing data
-    private List<Triplet<Person, Relationship, Person>> relationships = new ArrayList<>();
+    private final List<Triplet<Person, Relationship, Person>> relationships = new ArrayList<>();
 
     public List<Triplet<Person, Relationship, Person>> getRelationships() {
         return relationships;
