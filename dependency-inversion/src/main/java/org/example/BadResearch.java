71d943b4057fb876fa8bc142af514abbc39f3353
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 09:51:30 +0200

    Add BadResearch

---
+++ dependency-inversion/src/main/java/org/example/BadResearch.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class BadResearch {
+    private final BadRelationships relationships;
+
+    public BadResearch(BadRelationships badRelationships) {
+        this.relationships = badRelationships;
+    }
+
+    public void doResearch(String parentName) {
+        relationships.getRelationships().stream()
+                .filter(r -> r.getValue0().name().equals(parentName) && r.getValue1() == Relationship.PARENT)
+                .forEach(r -> System.out.println(parentName + " has a child called " + r.getValue2().name()));
+    }
+}
