e8b8e65aead30ada58225be33c15a81d05a1e3bc
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 08:54:59 +0200

    Add combining interface MultifunctionMachine

--- interface-segregation/src/main/java/org/example/interface_segregation/MultifunctionMachine.java
+++ interface-segregation/src/main/java/org/example/interface_segregation/MultifunctionMachine.java
@@ -3,5 +3,5 @@ package org.example.interface_segregation;
 import org.example.Document;
 
 public interface MultifunctionMachine extends Printer, Scanner {
-    public void copy(Document document);
+    void copy(Document document);
 }
