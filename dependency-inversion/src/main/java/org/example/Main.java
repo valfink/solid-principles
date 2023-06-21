fa435062e8ad4bb4551f6b89da9ebae7d68b7457
valfink <117605562+valfink@users.noreply.github.com>
2023-06-21 10:19:21 +0200

    Use new Classes

--- dependency-inversion/src/main/java/org/example/Main.java
+++ dependency-inversion/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.dependency_inversion.Relationships;
+import org.example.dependency_inversion.Research;
+
 public class Main {
     public static void main(String[] args) {
         Person john = new Person("John");
@@ -11,7 +14,17 @@ public class Main {
         badRelationships.addParentAndChild(john, sarah);
 
         // BAD: the high-level module depends on the low-level module and also on the implementation!
+        System.out.println("BAD WAY:");
         BadResearch badResearch = new BadResearch(badRelationships);
         badResearch.doResearch("John");
+
+        // GOOD
+        Relationships goodRelationships = new Relationships();
+        goodRelationships.addParentAndChild(john, chris);
+        goodRelationships.addParentAndChild(john, sarah);
+
+        System.out.println("GOOD WAY");
+        Research goodResearch = new Research(goodRelationships);
+        goodResearch.doResearch("John");
     }
 }
